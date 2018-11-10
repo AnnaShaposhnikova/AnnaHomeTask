83d086860a7dceb12bc34bcb2f489e6dfa9d6bdf
Anna Shaposhnikova <shaposhnikova.anna@gmail.com>
2018-11-10 15:39:25 +0200

    My eighth commit

--- src/com/hillel/hometask7/Methods.java
+++ src/com/hillel/hometask7/Methods.java
@@ -1,14 +1,46 @@
 package com.hillel.hometask7;
 
 public class Methods {
-//    public static int getMaxNum (int aIntNum []) {
-//        int max = 0;
-//        for (int i = 0; i< aIntNum.length; i++){
-//            if (i > i++){
-//               max = i;
-//            }
-//        }
-//        return max;
-//    }
+    public static int getMaxNum(int[] aIntNum) {
+        int max = aIntNum[0];
+        for (int i = 0; i < aIntNum.length; i++) {
+            if (aIntNum[i] > max) {
+                max = aIntNum[i];
+            }
+          }
+        return max;
     }
+
+    public static int getMinNum(int[] aIntNum) {
+        int min = aIntNum[0];
+        for (int i = 0; i < aIntNum.length; i++) {
+            if (aIntNum[i] < min) {
+                min = aIntNum[i];
+            }
+        }
+        return min;
+    }
+
+    public static double getMaxNum(double[] aDoubleNum) {
+        double max = aDoubleNum[0];
+        for (int i = 0; i < aDoubleNum.length; i++) {
+            if (aDoubleNum[i] > max) {
+                max = aDoubleNum[i];
+            }
+        }
+        return max;
+    }
+
+    public static double getMinNum(double[] aDoubleNum) {
+        double min = aDoubleNum[0];
+        for (int i = 0; i < aDoubleNum.length; i++) {
+            if (aDoubleNum[i] < min) {
+                min = aDoubleNum[i];
+            }
+        }
+        return min;
+    }
+
+
+}
 
--- src/com/hillel/hometask7/Start.java
+++ src/com/hillel/hometask7/Start.java
@@ -1,8 +1,21 @@
 package com.hillel.hometask7;
 
 public class Start {
-//    public static void main (String [] args){
-//        int max = Methods.getMaxNum(-10, 20, 34, -11, 100, 20);
-//        System.out.println("Max num is: " +max);
-//    }
+    public static void main (String [] args){
+        int[] aIntNums = {-10, 20, 34, -11, 100 , 20};
+        double[] aDoubleNums = {10.10, 43.543, 0.01, 1.3, 0.1, 43.4, 0.001 };
+
+        int max = Methods.getMaxNum(aIntNums);
+        System.out.println("Max int num is: " +max);
+
+        int min = Methods.getMinNum(aIntNums);
+        System.out.println("Min int num is: " +min);
+
+        double max1 = Methods.getMaxNum(aDoubleNums);
+        System.out.println("Max double num is: " +max1);
+
+        double min1 = Methods.getMinNum(aDoubleNums);
+        System.out.println("Min double num is: " +min1);
+
+    }
 }
